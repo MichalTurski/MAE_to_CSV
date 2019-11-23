cb672facd1cce1beeac21705ab5c699e3091cf2f
MichalTurski <michal.antoni.turski@gmail.com>
2019-11-23 11:53:07 +0100

    Relational form of sentences generator.

--- MAE_to_CSV.py
+++ MAE_to_CSV.py
@@ -4,7 +4,7 @@ import XML_parser.XML_parser as XML_parser
 import pandas as pd
 
 
-def sentence_generator(anot_df):
+def sentence_anots_generator(anot_df):
     is_sep = anot_df['category'] == 'SEPARATOR'
     sep_rows = [i for i, x in enumerate(is_sep) if x]
     sep_pairs = [(sep_rows[i] + 1, sep_rows[i + 1]) for i in range(len(sep_rows) - 1)]
@@ -12,21 +12,49 @@ def sentence_generator(anot_df):
     borders_list = [pair for pair in sep_pairs if anot_df.iloc[pair[0]]['xml_num'] == anot_df.iloc[pair[1]]['xml_num']]
     for borders in borders_list:
         yield anot_df.iloc[borders[0]:borders[1]]
-    print('end')
 
 
 def print_stats(anot_df):
-    for i, sentence in enumerate(sentence_generator(anot_df)):
-        active_actor_num = sum(sentence["category"] == 'Attribute')
-        deontic_num = sum(sentence["category"] == 'Deontic')
-        aim_num = sum(sentence["category"] == 'aIm')
-        object_num = sum(sentence["category"] == 'oBject')
-        passive_actor_num = sum(sentence["category"] == 'aCtor')
-        ac_num = sum(sentence["category"] == 'ActivCondition')
-        method_num = sum(sentence["category"] == 'Method')
+    for i, sentence in enumerate(sentence_anots_generator(anot_df)):
+        active_actor_num = sum(sentence['category'] == 'Attribute')
+        deontic_num = sum(sentence['category'] == 'Deontic')
+        aim_num = sum(sentence['category'] == 'aIm')
+        object_num = sum(sentence['category'] == 'oBject')
+        passive_actor_num = sum(sentence['category'] == 'aCtor')
+        ac_num = sum(sentence['category'] == 'ActivCondition')
+        method_num = sum(sentence['category'] == 'Method')
         print(f'In sentence {i + 1}: active actors num = {active_actor_num}, deontic num = {deontic_num}, '
-              f'object num = {object_num}, passive actor num = {passive_actor_num}, '
+              f'aim num = {aim_num}, object num = {object_num}, passive actor num = {passive_actor_num}, '
               f'active condition num = {ac_num}, method num = {method_num}')
+
+
+def get_objects(senetence_anots, obj_type):
+    emptyable = {'Deontic', 'ActivCondition', 'Method', 'oBject'}
+    df = senetence_anots.loc[senetence_anots['category'] == obj_type]
+    if obj_type in emptyable:
+        if df.empty:
+            df = df.append(pd.Series(), ignore_index=True)
+    return df
+
+def relational_sentence_genetator(anot_df):
+    # It returns each possible combination of entities (does a cartesian product). Therefore we call it Cogito XD.
+    for senetence_anots in sentence_anots_generator(anot_df):
+        active_actor_df = get_objects(senetence_anots, 'Attribute')
+        aim_df = get_objects(senetence_anots, 'aIm')
+        deontic_df = get_objects(senetence_anots, 'Deontic')
+        ac_df = get_objects(senetence_anots, 'ActivCondition')
+        method_df = get_objects(senetence_anots, 'Method')
+        passive_actor_df = get_objects(senetence_anots, 'aCtor')
+        object_df = get_objects(senetence_anots, 'oBject')
+        for active_actor in active_actor_df.iterrows():
+            for aim in aim_df.iterrows():
+                for deontic in deontic_df.iterrows():
+                    for ac in ac_df.iterrows():
+                        for method in method_df.iterrows():
+                            for passive_actor in passive_actor_df.iterrows():
+                                for obj in object_df.iterrows():
+                                    yield (active_actor, aim, deontic, ac, method, passive_actor, obj)
+
 
 @click.command()
 @click.argument('xml_directory', type=click.Path(exists=True))
@@ -41,7 +69,10 @@ def main(xml_directory):
         anot_dfs.append(anot_df)
     anot_df = pd.concat(anot_dfs, sort=False).reset_index(drop=True)
 
-    print_stats(anot_df)
+    # print_stats(anot_df)
+    for i, _ in enumerate(relational_sentence_genetator(anot_df)):
+        pass
+    print(i)
 
 
 if __name__ == '__main__':
