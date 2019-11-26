7e16c1c3990578aab1136c36394df77630c91d96
Stanislaw Pawlak <stanislawpawlak3@gmail.com>
2019-11-26 10:42:12 +0100

    refactor: product loop with itertools. tqdm added.

--- MAE_to_CSV.py
+++ MAE_to_CSV.py
@@ -1,7 +1,9 @@
+import itertools
 import os
 
 import click
 import pandas as pd
+from tqdm import tqdm
 
 import XML_parser.XML_parser as XML_parser
 from aim_category.aim_linker import AimLinker
@@ -54,17 +56,21 @@ def anot_text_generator(senetence_anots, anot_type):
 
 def relational_sentence_generator(anot_df, linker):
     # It returns each possible combination of entities (does a cartesian product). Therefore we call it Cogito XD.
-    for i, senetence_anots in enumerate(sentence_anots_generator(anot_df)):
-        for active_actor in anot_text_generator(senetence_anots, ATTRIBUTE_KEY):
-            for aim in anot_text_generator(senetence_anots, AIM_KEY):
-                for deontic in anot_text_generator(senetence_anots, DEONTIC_KEY):
-                    for ac in anot_text_generator(senetence_anots, ACTIVE_CONDITION_KEY):
-                        for method in anot_text_generator(senetence_anots, METHOD_KEY):
-                            for passive_actor in anot_text_generator(senetence_anots, ACTOR_KEY):
-                                for obj in anot_text_generator(senetence_anots, OBJECT_KEY):
-                                    # TODO: replace None with aim_category
-                                    yield (i + 1, active_actor, aim, linker.get_aim_category(aim), deontic, ac, method,
-                                           passive_actor, obj)
+    for i, senetence_anots in enumerate(tqdm(sentence_anots_generator(anot_df))):
+
+        for active_actor, aim, deontic, ac, method, passive_actor, obj in itertools.product(
+                *get_sentence_anots(senetence_anots)):
+            yield (i + 1, active_actor, aim, linker.get_aim_category(aim), deontic, ac, method, passive_actor, obj)
+
+
+def get_sentence_anots(senetence_anots):
+    return (anot_text_generator(senetence_anots, ATTRIBUTE_KEY),
+            anot_text_generator(senetence_anots, AIM_KEY),
+            anot_text_generator(senetence_anots, DEONTIC_KEY),
+            anot_text_generator(senetence_anots, ACTIVE_CONDITION_KEY),
+            anot_text_generator(senetence_anots, METHOD_KEY),
+            anot_text_generator(senetence_anots, ACTOR_KEY),
+            anot_text_generator(senetence_anots, OBJECT_KEY))
 
 
 def read_xmls(xml_directory):
@@ -83,6 +89,7 @@ def read_xmls(xml_directory):
 def create_aim_linker(df):
     aim_df = df.loc[df[CATEGORY_KEY] == AIM_KEY]
     return AimLinker(aim_df[['text']])
+
 
 @click.command()
 @click.argument('xml_directory', type=click.Path(exists=True))
